46c4e41b14046a0d5f88b5d47bc30c188b180e16
Christoph Stach <christoph.stach@gmail.com>
2022-11-16 15:10:53 +0100

    Change input files

--- src/main/kotlin/de/htw/berlin/covidspings/Application.kt
+++ src/main/kotlin/de/htw/berlin/covidspings/Application.kt
@@ -23,34 +23,34 @@ fun main() {
             includeSitesWithoutReads = false
         )
 
-        val subElapsed1 = measureTimeMillis { variantCaller.processBam("input/input-1/input.bam") }
+        val subElapsed1 = measureTimeMillis { variantCaller.processBam("input/real/input_001.bam") }
         println("1 $subElapsed1")
 
-        val subElapsed2 = measureTimeMillis { variantCaller.processBam("input/input-2/input.bam") }
+        val subElapsed2 = measureTimeMillis { variantCaller.processBam("input/real/input_002.bam") }
         println("2 $subElapsed2")
 
-        val subElapsed3 = measureTimeMillis { variantCaller.processBam("input/input-3/input.bam") }
+        val subElapsed3 = measureTimeMillis { variantCaller.processBam("input/real/input_003.bam") }
         println("3 $subElapsed3")
 
-        val subElapsed4 = measureTimeMillis { variantCaller.processBam("input/input-4/input.bam") }
+        val subElapsed4 = measureTimeMillis { variantCaller.processBam("input/real/input_004.bam") }
         println("4 $subElapsed4")
 
-        val subElapsed5 = measureTimeMillis { variantCaller.processBam("input/input-5/input.bam") }
+        val subElapsed5 = measureTimeMillis { variantCaller.processBam("input/real/input_005.bam") }
         println("5 $subElapsed5")
 
-        val subElapsed6 = measureTimeMillis { variantCaller.processBam("input/input-6/input.bam") }
+        val subElapsed6 = measureTimeMillis { variantCaller.processBam("input/real/input_006.bam") }
         println("6 $subElapsed6")
 
-        val subElapsed7 = measureTimeMillis { variantCaller.processBam("input/input-7/input.bam") }
+        val subElapsed7 = measureTimeMillis { variantCaller.processBam("input/real/input_007.bam") }
         println("7 $subElapsed7")
 
-        val subElapsed8 = measureTimeMillis { variantCaller.processBam("input/input-8/input.bam") }
+        val subElapsed8 = measureTimeMillis { variantCaller.processBam("input/real/input_008.bam") }
         println("8 $subElapsed8")
 
-        val subElapsed9 = measureTimeMillis { variantCaller.processBam("input/input-9/input.bam") }
+        val subElapsed9 = measureTimeMillis { variantCaller.processBam("input/real/input_009.bam") }
         println("9 $subElapsed9")
 
-        val subElapsed10 = measureTimeMillis { variantCaller.processBam("input/input-10/input.bam") }
+        val subElapsed10 = measureTimeMillis { variantCaller.processBam("input/real/input_010.bam") }
         println("10 $subElapsed10")
 
         variantCaller.writeVcf("output/output.vcf")
