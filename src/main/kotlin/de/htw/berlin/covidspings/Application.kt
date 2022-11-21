0c6ab8ab59150a6c442585197f4bc4e382c579f4
Christoph Stach <christoph.stach@gmail.com>
2022-11-21 08:37:45 +0100

    Better time measures

--- src/main/kotlin/de/htw/berlin/covidspings/Application.kt
+++ src/main/kotlin/de/htw/berlin/covidspings/Application.kt
@@ -1,62 +1,62 @@
 package de.htw.berlin.covidspings
 
-import kotlin.system.measureTimeMillis
+import java.time.Instant
 
 fun main() {
-    val elapsed = measureTimeMillis {
-        val minAlleleDepth = 5
-        val minEvidenceRatio = 0.0f
-        val maxVariants = 1
-        val minTotalDepth = 10
-        val minMappingQuality: Byte = 20
-        val minBaseQuality: Byte = 30
+    val start = Instant.now().toEpochMilli()
 
-        val variantCaller = RealtimeVariantCaller(
-            "input/reference.fasta",
-            minBaseQuality,
-            minMappingQuality,
-            minTotalDepth,
-            minAlleleDepth,
-            minEvidenceRatio,
-            maxVariants,
-            filterDuplicates = true,
-            includeSitesWithoutReads = false
-        )
+    val minAlleleDepth = 5
+    val minEvidenceRatio = 0.0f
+    val maxVariants = 1
+    val minTotalDepth = 10
+    val minMappingQuality: Byte = 20
+    val minBaseQuality: Byte = 30
 
-        val subElapsed1 = measureTimeMillis { variantCaller.processBam("input/real/input_001.bam") }
-        println("1 $subElapsed1")
+    val variantCaller = RealtimeVariantCaller(
+        "input/reference.fasta",
+        minBaseQuality,
+        minMappingQuality,
+        minTotalDepth,
+        minAlleleDepth,
+        minEvidenceRatio,
+        maxVariants,
+        filterDuplicates = true,
+        includeSitesWithoutReads = false
+    )
 
-        val subElapsed2 = measureTimeMillis { variantCaller.processBam("input/real/input_002.bam") }
-        println("2 $subElapsed2")
+    variantCaller.processBam("input/real/input_001.bam")
+    println("${Instant.now().toEpochMilli() - start}")
 
-        val subElapsed3 = measureTimeMillis { variantCaller.processBam("input/real/input_003.bam") }
-        println("3 $subElapsed3")
+    variantCaller.processBam("input/real/input_002.bam")
+    println("${Instant.now().toEpochMilli() - start}")
 
-        val subElapsed4 = measureTimeMillis { variantCaller.processBam("input/real/input_004.bam") }
-        println("4 $subElapsed4")
+    variantCaller.processBam("input/real/input_003.bam")
+    println("${Instant.now().toEpochMilli() - start}")
 
-        val subElapsed5 = measureTimeMillis { variantCaller.processBam("input/real/input_005.bam") }
-        println("5 $subElapsed5")
+    variantCaller.processBam("input/real/input_004.bam")
+    println("${Instant.now().toEpochMilli() - start}")
 
-        val subElapsed6 = measureTimeMillis { variantCaller.processBam("input/real/input_006.bam") }
-        println("6 $subElapsed6")
+    variantCaller.processBam("input/real/input_005.bam")
+    println("${Instant.now().toEpochMilli() - start}")
 
-        val subElapsed7 = measureTimeMillis { variantCaller.processBam("input/real/input_007.bam") }
-        println("7 $subElapsed7")
+    variantCaller.processBam("input/real/input_006.bam")
+    println("${Instant.now().toEpochMilli() - start}")
 
-        val subElapsed8 = measureTimeMillis { variantCaller.processBam("input/real/input_008.bam") }
-        println("8 $subElapsed8")
+    variantCaller.processBam("input/real/input_007.bam")
+    println("${Instant.now().toEpochMilli() - start}")
 
-        val subElapsed9 = measureTimeMillis { variantCaller.processBam("input/real/input_009.bam") }
-        println("9 $subElapsed9")
+    variantCaller.processBam("input/real/input_008.bam")
+    println("${Instant.now().toEpochMilli() - start}")
 
-        val subElapsed10 = measureTimeMillis { variantCaller.processBam("input/real/input_010.bam") }
-        println("10 $subElapsed10")
+    variantCaller.processBam("input/real/input_009.bam")
+    println("${Instant.now().toEpochMilli() - start}")
 
-        variantCaller.writeVcf("output/output.vcf")
-        // variantCaller.writeJson("output/output.json")
+    variantCaller.processBam("input/real/input_010.bam")
+    println("${Instant.now().toEpochMilli() - start}")
 
-    }
+    variantCaller.writeVcf("output/output.vcf")
+    // variantCaller.writeJson("output/output.json")
 
-    println("Total $elapsed")
+
+    println("Total ${Instant.now().toEpochMilli() - start}")
 }
