be9001fd200e5a82c4876e12c250166994297ad5
Christoph Stach <christoph.stach@gmail.com>
2022-11-23 11:12:48 +0100

    Measure data for second plot

--- src/main/kotlin/de/htw/berlin/covidspings/Application.kt
+++ src/main/kotlin/de/htw/berlin/covidspings/Application.kt
@@ -3,7 +3,7 @@ package de.htw.berlin.covidspings
 import java.time.Instant
 
 fun main() {
-    val start = Instant.now().toEpochMilli()
+    // val start = Instant.now().toEpochMilli()
 
     val minAlleleDepth = 5
     val minEvidenceRatio = 0.0f
@@ -24,39 +24,43 @@ fun main() {
         includeSitesWithoutReads = false
     )
 
-    variantCaller.processBam("input/real/input_001.bam")
+    val start = Instant.now().toEpochMilli()
+    variantCaller.processBam("input/input-10/input.bam")
     println("${Instant.now().toEpochMilli() - start}")
 
-    variantCaller.processBam("input/real/input_002.bam")
-    println("${Instant.now().toEpochMilli() - start}")
+    // variantCaller.processBam("input/real/input_001.bam")
+    // println("${Instant.now().toEpochMilli() - start}")
 
-    variantCaller.processBam("input/real/input_003.bam")
-    println("${Instant.now().toEpochMilli() - start}")
+    // variantCaller.processBam("input/real/input_002.bam")
+    // println("${Instant.now().toEpochMilli() - start}")
 
-    variantCaller.processBam("input/real/input_004.bam")
-    println("${Instant.now().toEpochMilli() - start}")
+    // variantCaller.processBam("input/real/input_003.bam")
+    // println("${Instant.now().toEpochMilli() - start}")
 
-    variantCaller.processBam("input/real/input_005.bam")
-    println("${Instant.now().toEpochMilli() - start}")
+    // variantCaller.processBam("input/real/input_004.bam")
+    // println("${Instant.now().toEpochMilli() - start}")
 
-    variantCaller.processBam("input/real/input_006.bam")
-    println("${Instant.now().toEpochMilli() - start}")
+    // variantCaller.processBam("input/real/input_005.bam")
+    // println("${Instant.now().toEpochMilli() - start}")
 
-    variantCaller.processBam("input/real/input_007.bam")
-    println("${Instant.now().toEpochMilli() - start}")
+    // variantCaller.processBam("input/real/input_006.bam")
+    // println("${Instant.now().toEpochMilli() - start}")
 
-    variantCaller.processBam("input/real/input_008.bam")
-    println("${Instant.now().toEpochMilli() - start}")
+    // variantCaller.processBam("input/real/input_007.bam")
+    // println("${Instant.now().toEpochMilli() - start}")
 
-    variantCaller.processBam("input/real/input_009.bam")
-    println("${Instant.now().toEpochMilli() - start}")
+    // variantCaller.processBam("input/real/input_008.bam")
+    // println("${Instant.now().toEpochMilli() - start}")
 
-    variantCaller.processBam("input/real/input_010.bam")
-    println("${Instant.now().toEpochMilli() - start}")
+    // variantCaller.processBam("input/real/input_009.bam")
+    // println("${Instant.now().toEpochMilli() - start}")
 
-    variantCaller.writeVcf("output/output.vcf")
+    // variantCaller.processBam("input/real/input_010.bam")
+    // println("${Instant.now().toEpochMilli() - start}")
+
+    // variantCaller.writeVcf("output/output.vcf")
     // variantCaller.writeJson("output/output.json")
 
 
-    println("Total ${Instant.now().toEpochMilli() - start}")
+    // println("Total ${Instant.now().toEpochMilli() - start}")
 }
